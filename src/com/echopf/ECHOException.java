dd226efd5e4c4b6fb9e8df8c3448dfbafb79b6e9
Yasuhiro Inuma <yasuhiro.inuma@neurobase.link>
2015-07-10 16:51:41 +0900

    Added the definition of error codes.

--- src/com/echopf/ECHOException.java
+++ src/com/echopf/ECHOException.java
@@ -32,8 +32,34 @@ public class ECHOException extends Exception {
 	private int code;
 	private Map<String, Throwable> details = null;
 
-	public static int RESOURCE_NOT_FOUND = 110010;
-	public static int INVALID_JSON_FORMAT = 110050;
+    
+    /* Begin error codes */
+    
+    public static int RESOURCE_NOT_FOUND = 110010;
+    public static int NOTFOUND_OR_FORBIDDEN = 110020;
+    public static int METHOD_NOT_ALLOWED = 110030;
+    public static int UNSUPPORTED_MEDIA_TYPE = 110040;
+    public static int INVALID_JSON_FORMAT = 110050;
+    public static int APPID_NOT_SPECIFIED = 100010;
+    public static int APPKEY_NOT_SPECIFIED = 100020;
+    public static int INVALID_API_APPLICATION = 100030;
+    public static int OPERATION_NOT_PERMITTED = 130000;
+    public static int AUTHENTICATION_ERROR = 130010;
+    public static int ACCESSTOKEN_INCORRECTED_OR_EXPIRED = 130020;
+    public static int VALIDATION_ERRORS_OCCURRED = 150000;
+    public static int NOT_SETTED = 150010;
+    public static int CONTAINED_RESTRICTED_CHARACTER = 150020;
+    public static int TOO_LONG = 150030;
+    public static int NON_UNIQUE = 150040;
+    public static int CONTAINED_NON_NUMERIC_CHARACTER = 150050;
+    public static int INVALID_EMAIL_FORMAT = 150060;
+    public static int REFERENCE_LOOPED = 150070;
+    public static int INVALID_PHONE_NUMBER_FORMAT = 150080;
+    public static int INVALID_DATE_STRING_FORMAT = 150090;
+    public static int INVALID_ZIPCODE_FORMAT = 150100;
+    public static int REFERENCE_NOT_EXIST = 150110;
+    
+    /* End error codes */
 	
 	
 	/* Begin constructors */
