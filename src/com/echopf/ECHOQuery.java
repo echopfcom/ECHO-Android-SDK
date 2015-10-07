778775f24762ba2da8681032c36409fa8d515167
Yasuhiro Inuma <yasuhiro.inuma@neurobase.link>
2015-10-07 11:43:48 +0900

    Disused the finding loop when the limit is not specified in params.

--- src/com/echopf/ECHOQuery.java
+++ src/com/echopf/ECHOQuery.java
@@ -58,20 +58,8 @@ public class ECHOQuery {
 	 * @throws ECHOException
 	 */
 	public static <T extends ECHODataObject<T>> List<T> doFind(final boolean sync, final String listKey, final String resourceType, 
-			final FindCallback<T> callback, final String instanceId, JSONObject params,
+			final FindCallback<T> callback, final String instanceId, final JSONObject fParams,
 			final ECHODataObjectFactory<T> factory) throws ECHOException {
-		
-		// set default params
-		if(params == null) params = new JSONObject();
-		final boolean getall =  (params.optInt("limit") == 0);
-		
-		try {
-			if(getall) params.put("limit", Integer.MAX_VALUE);
-		} catch (JSONException e) {
-			throw new InternalError();
-		} 
-		final JSONObject fParams = params;
-		
 		
 		// Get ready a background thread
 		final Handler handler = new Handler();
@@ -83,50 +71,24 @@ public class ECHOQuery {
 					ECHOException exception = null;
 					
 					try {
-						int currentPageNo = 0, currentPageCount;
+						JSONObject response = getRequest(instanceId + "/" + resourceType , fParams);
+
+						/* begin copying data */
+						JSONArray items = response.optJSONArray(listKey);
+						if(items == null) throw new ECHOException(0, "The copying data is not acceptable. That is why a records field is not specified.");
 						
-						do {
-							JSONObject response = getRequest(instanceId + "/" + resourceType , fParams);
-
-							/* begin copying data */
-							JSONArray items = response.optJSONArray(listKey);
-							if(items == null) throw new ECHOException(0, "The copying data is not acceptable. That is why a records field is not specified.");
+						for (int i = 0; i < items.length(); i++) {
+							JSONObject item = items.optJSONObject(i);
+							if(item == null) throw new ECHOException(0, "The copying data is not acceptable. That is why a members field is not specified.");
 							
-							for (int i = 0; i < items.length(); i++) {
-								JSONObject item = items.optJSONObject(i);
-								if(item == null) throw new ECHOException(0, "The copying data is not acceptable. That is why a members field is not specified.");
-								
-								String refid = item.optString("refid");
-								if(refid.isEmpty()) throw new ECHOException(0, "The copying data is not acceptable. That is why a refid is not specified.");
-	
-								T obj = factory.create(instanceId, refid, item);
-								objList.add(obj);
-							}
-							/* end copying data */
+							String refid = item.optString("refid");
+							if(refid.isEmpty()) throw new ECHOException(0, "The copying data is not acceptable. That is why a refid is not specified.");
 
+							T obj = factory.create(instanceId, refid, item);
+							objList.add(obj);
+						}
+						/* end copying data */
 
-							// begin pagination
-							if(getall == false) break;
-							
-							JSONObject paginate = response.optJSONObject("paginate");
-							if(paginate == null) throw new ECHOException(0, "The copying data is not acceptable.");
-							
-							if(currentPageNo+1 != paginate.optInt("page")) throw new ECHOException(0, "The copying data is not acceptable.");
-							currentPageNo = paginate.optInt("page");
-							if(currentPageNo == 0) throw new ECHOException(0, "The copying data is not acceptable.");
-
-							currentPageCount = paginate.optInt("pageCount");
-							if(currentPageCount == 0) throw new ECHOException(0, "The copying data is not acceptable.");
-							
-							if(currentPageNo >= currentPageCount) break;
-							
-							try {
-								fParams.put("page", currentPageNo+1);
-							} catch (JSONException e) {
-								throw new InternalError();
-							}
-
-						} while(true);
 					} catch (ECHOException e) {
 						exception = e;
 					} catch (RuntimeException e) {
