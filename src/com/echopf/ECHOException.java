5ae0ff62ba9c8f1a374214ecb8f4db33ccff98c8
Yasuhiro Inuma <yasuhiro.inuma@neurobase.link>
2015-10-19 12:17:24 +0900

    Changed the Throwable into the ECHOException in the value of error details.

--- src/com/echopf/ECHOException.java
+++ src/com/echopf/ECHOException.java
@@ -30,36 +30,36 @@ public class ECHOException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 	private int code;
-	private Map<String, Throwable> details = null;
+	private Map<String, ECHOException> details = null;
 
-    
-    /* Begin error codes */
-    
-    public static int RESOURCE_NOT_FOUND = 110010;
-    public static int NOTFOUND_OR_FORBIDDEN = 110020;
-    public static int METHOD_NOT_ALLOWED = 110030;
-    public static int UNSUPPORTED_MEDIA_TYPE = 110040;
-    public static int INVALID_JSON_FORMAT = 110050;
-    public static int APPID_NOT_SPECIFIED = 100010;
-    public static int APPKEY_NOT_SPECIFIED = 100020;
-    public static int INVALID_API_APPLICATION = 100030;
-    public static int OPERATION_NOT_PERMITTED = 130000;
-    public static int AUTHENTICATION_ERROR = 130010;
-    public static int ACCESSTOKEN_INCORRECTED_OR_EXPIRED = 130020;
-    public static int VALIDATION_ERRORS_OCCURRED = 150000;
-    public static int NOT_SETTED = 150010;
-    public static int CONTAINED_RESTRICTED_CHARACTER = 150020;
-    public static int TOO_LONG = 150030;
-    public static int NON_UNIQUE = 150040;
-    public static int CONTAINED_NON_NUMERIC_CHARACTER = 150050;
-    public static int INVALID_EMAIL_FORMAT = 150060;
-    public static int REFERENCE_LOOPED = 150070;
-    public static int INVALID_PHONE_NUMBER_FORMAT = 150080;
-    public static int INVALID_DATE_STRING_FORMAT = 150090;
-    public static int INVALID_ZIPCODE_FORMAT = 150100;
-    public static int REFERENCE_NOT_EXIST = 150110;
-    
-    /* End error codes */
+	
+	/* Begin error codes */
+	
+	public static int RESOURCE_NOT_FOUND = 110010;
+	public static int NOTFOUND_OR_FORBIDDEN = 110020;
+	public static int METHOD_NOT_ALLOWED = 110030;
+	public static int UNSUPPORTED_MEDIA_TYPE = 110040;
+	public static int INVALID_JSON_FORMAT = 110050;
+	public static int APPID_NOT_SPECIFIED = 100010;
+	public static int APPKEY_NOT_SPECIFIED = 100020;
+	public static int INVALID_API_APPLICATION = 100030;
+	public static int OPERATION_NOT_PERMITTED = 130000;
+	public static int AUTHENTICATION_ERROR = 130010;
+	public static int ACCESSTOKEN_INCORRECTED_OR_EXPIRED = 130020;
+	public static int VALIDATION_ERRORS_OCCURRED = 150000;
+	public static int NOT_SETTED = 150010;
+	public static int CONTAINED_RESTRICTED_CHARACTER = 150020;
+	public static int TOO_LONG = 150030;
+	public static int NON_UNIQUE = 150040;
+	public static int CONTAINED_NON_NUMERIC_CHARACTER = 150050;
+	public static int INVALID_EMAIL_FORMAT = 150060;
+	public static int REFERENCE_LOOPED = 150070;
+	public static int INVALID_PHONE_NUMBER_FORMAT = 150080;
+	public static int INVALID_DATE_STRING_FORMAT = 150090;
+	public static int INVALID_ZIPCODE_FORMAT = 150100;
+	public static int REFERENCE_NOT_EXIST = 150110;
+
+	/* End error codes */
 	
 	
 	/* Begin constructors */
@@ -80,7 +80,7 @@ public class ECHOException extends Exception {
 	    this(code, message);
 	    
 	    if(details != null) {
-		    this.details = new HashMap<String, Throwable>();
+		    this.details = new HashMap<String, ECHOException>();
 	
 		    Iterator<?> iter = details.keys();
 			while (iter.hasNext()) {
@@ -126,7 +126,7 @@ public class ECHOException extends Exception {
 	/**
 	 * Gets detail information.
 	 */
-	public Map<String, Throwable> getDetails() {
+	public Map<String, ECHOException> getDetails() {
 		return this.details;
 	}
 }
